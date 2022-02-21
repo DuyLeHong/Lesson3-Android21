a0dd6fca344d0957c51ad1973b5dbc266f3f153f
Duy Le <lehongduy.bk@gmail.com>
2022-02-21 20:24:38 +0700

    Lesson 3 - FrameLayout

--- app/src/main/java/com/example/lesson3/MainActivity.java
+++ app/src/main/java/com/example/lesson3/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.lesson3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.framelayout_screen);
+
+        Button btnClick = findViewById(R.id.btn_click);
+
+        ImageView ivBg = findViewById(R.id.imv_bg);
+
+
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ivBg.getVisibility() == View.VISIBLE) {
+                    ivBg.setVisibility(View.GONE);
+                } else {
+                    ivBg.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 }
